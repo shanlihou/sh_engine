1605e59124978588a68bb2ce723e54427d3aadea
shanlihou <shanlihou@gmail.com>
2022-09-14 14:13:03 +0800

    up

---
+++ demos/3.1/include/demo_3_1.h
@@ -0,0 +1,7 @@
+#ifndef DEMO_3_1_H
+#define DEMO_3_1_H
+
+namespace DEMO_3_1{
+    int run();
+}
+#endif
--- gl_test.cpp
+++ gl_test.cpp
@@ -2,6 +2,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "demo_3_1.h"
 
 // int main()
 // {
@@ -10,33 +11,35 @@
 //
 int main(void)
 {
-    GLFWwindow* window;
-    /* 初始化glfw */
-    if (!glfwInit())
-        return -1;
-    /* 创建一个Window 和 OpenGL上下文 */
-    window = glfwCreateWindow(960, 640, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        //创建失败就退出
-        glfwTerminate();
-        return -1;
-    }
-    /* 激活上面创建的OpenGL上下文 */
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
-    /* 进入游戏引擎主循环 */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-        glClearColor(49.f/255,77.f/255,121.f/255,1.f);
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-        /* 处理鼠标 键盘事件 */
-        glfwPollEvents();
-    }
-    glfwTerminate();
+    std::cout <<"hello";
+    DEMO_3_1::run();
+    // GLFWwindow* window;
+    // /* 初始化glfw */
+    // if (!glfwInit())
+    //     return -1;
+    // /* 创建一个Window 和 OpenGL上下文 */
+    // window = glfwCreateWindow(960, 640, "Hello World", NULL, NULL);
+    // if (!window)
+    // {
+    //     //创建失败就退出
+    //     glfwTerminate();
+    //     return -1;
+    // }
+    // /* 激活上面创建的OpenGL上下文 */
+    // glfwMakeContextCurrent(window);
+    // gladLoadGL();
+    // /* 进入游戏引擎主循环 */
+    // while (!glfwWindowShouldClose(window))
+    // {
+    //     /* Render here */
+    //     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    //     glClearColor(49.f/255,77.f/255,121.f/255,1.f);
+    //     /* Swap front and back buffers */
+    //     glfwSwapBuffers(window);
+    //     /* 处理鼠标 键盘事件 */
+    //     glfwPollEvents();
+    // }
+    // glfwTerminate();
     return 0;
 }
 
